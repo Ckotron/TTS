213f79e693a71e1ba0fca8438f45568cb62ff272
Ckotron <czuhernandez@gmail.com>
2019-11-10 16:17:07 -0600

    Bug arreglado

--- src/controllers/pacientesController.js
+++ src/controllers/pacientesController.js
@@ -5,23 +5,15 @@ controller.list = (req,res) =>{
 	idDocPac = req.params.idDoc;
 	console.log(idDocPac+"list");
 	req.getConnection((err,conn) =>{
-		conn.query('SELECT D.idDoctor, P.idPaciente, P.Nombre, P.ApPaterno, P.ApMaterno FROM Doctores D LEFT JOIN Pacientes P ON D.idDoctor = P.idDoctor WHERE D.idDoctor = ? AND Estado="Activo"',[idDocPac], (err, pacientes) =>{
+		conn.query('SELECT D.idDoctor, P.idPaciente, P.Nombre, P.ApPaterno, P.ApMaterno FROM Doctores D LEFT JOIN Pacientes P ON D.idDoctor = P.idDoctor WHERE D.idDoctor = ? AND Estado="Activo" UNION SELECT "" AS "idDoctor", "" AS "idPaciente", "" AS "Nombre", "" AS "ApPaterno", "" AS "ApMaterno"',[idDocPac], (err, pacientes) =>{
 			if(err){
 				res.json(err);
 			}
 			console.log(pacientes);
-			//if(pacientes != null){
-				res.render('pacientes', {
-					data: pacientes
-				});
-			//	console.log(pacientes);
-			//}else{
-			//	pacientes=idDoc;
-			//	console.log(idDocPac+"pruebaif");
-			//	res.render('pacientes', {
-			//		data: idDocPac
-			//	});
-			//}
+			res.render('pacientes', {
+				data: pacientes,
+				doc: idDocPac
+			});
 		});
 	});
 };
