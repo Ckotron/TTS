d36662856deb9ccb2c7b6e43e37f4b88f2e1c959
Ckotron <czuhernandez@gmail.com>
2019-11-10 21:19:16 -0600

    Arreglo y mejora en la generación del reporte

--- src/routes/generar_reporte.js
+++ src/routes/generar_reporte.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const generar_reporteController = require('../controllers/generar_reporteController.js');
 
 router.get('/:idDoc/atender.ejs/generar_reporte.ejs/:idPac/:idFch', generar_reporteController.list);
-router.post('/:idDoc/atender.ejs/:idPac/:idFch', generar_reporteController.update);
+router.post('/:idDoc/atender.ejs/:idPac/:idFch/saveRecom', generar_reporteController.update);
 
 module.exports = router;
